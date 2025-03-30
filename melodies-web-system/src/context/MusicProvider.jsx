50c25d6bc0cd337730da4c0ecee1754027afbd26
Co Hung <trancohung2016@gmail.com>
2025-03-30 20:08:37 +0700

    search

--- melodies-web-system/src/context/MusicProvider.jsx
+++ melodies-web-system/src/context/MusicProvider.jsx
@@ -32,12 +32,6 @@ export const MusicProvider = ({ children }) => {
       setIsPlaying(true);
     }
 
-    if (!currentSong) {
-      audioRef.pause();
-      audioRef.currentTime = 0;
-      setIsPlaying(false);
-    }
-
     const handleSongEnd = () => {
       if (isRepeatingRef.current) {
         audio.currentTime = 0;
